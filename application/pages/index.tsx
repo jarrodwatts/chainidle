1d9831fb8a30c3993f3ea4e3a55c270c2fe166a9
Jarrod Watts <jarrodwatts16@gmail.com>
2022-07-10 23:22:48 +1000

    Add ethers

--- application/pages/index.tsx
+++ application/pages/index.tsx
@@ -15,7 +15,51 @@ const Home: NextPage = () => {
           alignItems: "center",
         }}
       >
-        <Typography variant="h1">Coming Soon!</Typography>
+        <Typography
+          variant="h1"
+          sx={{
+            mb: 4,
+          }}
+        >
+          Chain Idle!
+        </Typography>
+
+        <Typography variant="body1" sx={{ mb: 4 }}>
+          An open-source, play-to-earn idle game where you improve your
+          character&apos;s skills and earn resources for your hard work.
+        </Typography>
+
+        <ul>
+          <li>
+            <Typography variant="body1">
+              Mint ERC-721 NFT characters with upgradable skills on-chain
+            </Typography>
+          </li>
+          <li>
+            <Typography variant="body1">
+              Buy ERC-1155 NFT tools from the shop
+            </Typography>
+          </li>
+          <li>
+            <Typography variant="body1">
+              Stake your character and tool on a contract to start earning
+              ERC-20 rewards
+            </Typography>
+          </li>
+          <li>
+            <Typography variant="body1">
+              Craft unique ERC-721 "upgraded tools" from the forgery
+            </Typography>
+          </li>
+          <li>
+            <Typography variant="body1">
+              Sell your tools on the marketplace - Open loot boxes with parts,
+              tools and more
+            </Typography>
+          </li>
+        </ul>
+
+        <a href="https://github.com/jarrodwatts/chainidle">View the GitHub</a>
       </Box>
     </Container>
   );
