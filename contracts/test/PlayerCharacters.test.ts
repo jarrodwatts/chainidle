14d15538900596cede0975a11540fc8a19456939
Jarrod Watts <jarrodwatts16@gmail.com>
2022-07-24 14:35:31 +1000

    Player Characters tests

--- contracts/test/PlayerCharacters.test.ts
+++ contracts/test/PlayerCharacters.test.ts
@@ -13,7 +13,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 // `describe` receives the name of a section of your test suite, and a
 // callback. The callback must define the tests of that section. This callback
 // can't be an async function.
-describe("Token contract", function () {
+describe("Player Characters", function () {
   // We define a fixture to reuse the same setup in every test. We use
   // loadFixture to run this setup once, snapshot that state, and reset Hardhat
   // Network to that snapshopt in every test.
@@ -37,65 +37,76 @@ describe("Token contract", function () {
     return { PlayerCharactersFactory, playerCharacters, owner, addr1, addr2 };
   }
 
-  // You can nest describe calls to create subsections.
-  describe("Player Characters", function () {
-    // `it` is another Mocha function. This is the one you use to define each
-    // of your tests. It receives the test name, and a callback function.
-    //
-    // If the callback function is async, Mocha will `await` it.
-    it("Should deploy the contract", async function () {
-      // We use loadFixture to setup our environment, and then assert that
-      // things went well
-      const { playerCharacters, owner } = await loadFixture(
-        deployPlayerCharactersFixture
-      );
+  // `it` is another Mocha function. This is the one you use to define each
+  // of your tests. It receives the test name, and a callback function.
+  //
+  // If the callback function is async, Mocha will `await` it.
+  it("Should deploy the contract", async function () {
+    // We use loadFixture to setup our environment, and then assert that
+    // things went well
+    const { playerCharacters, owner } = await loadFixture(
+      deployPlayerCharactersFixture
+    );
 
-      // `expect` receives a value and wraps it in an assertion object. These
-      // objects have a lot of utility methods to assert values.
+    // `expect` receives a value and wraps it in an assertion object. These
+    // objects have a lot of utility methods to assert values.
 
-      // This test expects the owner variable stored in the contract to be
-      // equal to our Signer's owner.
-      expect(await playerCharacters.owner()).to.equal(owner.address);
-    });
+    // This test expects the owner variable stored in the contract to be
+    // equal to our Signer's owner.
+    expect(await playerCharacters.owner()).to.equal(owner.address);
+  });
 
-    it("Should mint a character to a wallet", async function () {
-      const { playerCharacters, owner, addr1 } = await loadFixture(
-        deployPlayerCharactersFixture
-      );
+  it("Should mint a character to a wallet", async function () {
+    const { playerCharacters, owner, addr1 } = await loadFixture(
+      deployPlayerCharactersFixture
+    );
 
-      const tx = await playerCharacters.mintTo(addr1.address, "uri");
+    const tx = await playerCharacters.mintTo(addr1.address, "uri");
 
-      const bal = await playerCharacters.balanceOf(addr1.address);
+    const bal = await playerCharacters.balanceOf(addr1.address);
 
-      expect(bal).to.equal(1);
-    });
+    expect(bal).to.equal(1);
+  });
 
-    it("Should reject non-admin wallet from updating player XP", async function () {
-      const { playerCharacters, owner, addr1 } = await loadFixture(
-        deployPlayerCharactersFixture
-      );
+  it("Should reject non-admin wallet from updating player XP", async function () {
+    const { playerCharacters, owner, addr1 } = await loadFixture(
+      deployPlayerCharactersFixture
+    );
 
-      const expectedRejectRequest = await expect(
-        playerCharacters.connect(addr1).updatePlayerSkill(0, "MINING", 10)
-      ).to.be.reverted;
-    });
+    const expectedRejectRequest = await expect(
+      playerCharacters.connect(addr1).updatePlayerSkill(0, "MINING", 10)
+    ).to.be.reverted;
+  });
 
-    it("Should update player XP from an admin wallet", async function () {
-      const { playerCharacters, owner, addr1 } = await loadFixture(
-        deployPlayerCharactersFixture
-      );
+  it("Should update player XP from an admin wallet", async function () {
+    const { playerCharacters, owner, addr1 } = await loadFixture(
+      deployPlayerCharactersFixture
+    );
 
-      const tx = await playerCharacters
-        .connect(owner)
-        .updatePlayerSkill(addr1.address, "MINING", 10);
+    const tx = await playerCharacters
+      .connect(owner)
+      .updatePlayerSkill(addr1.address, "MINING", 10);
 
-      // Read player skill
-      const skill = await playerCharacters.playerSkillExperience(
-        addr1.address,
-        "MINING"
-      );
+    const skill = await playerCharacters.playerSkillExperience(
+      addr1.address,
+      "MINING"
+    );
 
-      expect(skill).to.equal(10);
-    });
+    expect(skill).to.equal(10);
+
+    const tx2 = await playerCharacters
+      .connect(owner)
+      .updatePlayerSkill(addr1.address, "MINING", 10);
+
+    const tx3 = await playerCharacters
+      .connect(owner)
+      .updatePlayerSkill(addr1.address, "MINING", 10);
+
+    const skillNow = await playerCharacters.playerSkillExperience(
+      addr1.address,
+      "MINING"
+    );
+
+    expect(skillNow).to.equal(30);
   });
 });
